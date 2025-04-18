a54a49746234d417514c7396ecdcac7f0ae9f06e
Amm2010 <ammar2010428@gmail.com>
2025-04-18 21:43:22 +0300

    save 3

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ var equation = "";
 function addNumber(number) {
     equation += number;
     resultdiv.innerText = equation; 
+    var displayText = equation.replace( '*' , "×");
+    equation = displayText;
 }
 
-function divid() {
-    
-}
+
+
 
 function calculate() {
     result = eval(equation);
     equation = result;
     resultdiv.innerText = result;
+
 }
 
 function deletAll() {
     equation = "";
     resultdiv.innerText = 0;
+}
+
+function deletLast() {
+    equation = equation.slice(0, -1);
+    resultdiv.innerText = equation || "0";
+    var displayText = equation.replace( '*' , "×");
+    equation = displayText;
 }
